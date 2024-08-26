73ff26c0878a1cb3e2aaa14aadae8bc71fa3010a
Mehvish-A <167060486+Mehvish-A@users.noreply.github.com>
2024-08-26 15:51:31 +0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,21 +1,85 @@
-import { useEffect } from "react";
-import {useDispatch, useSelector} from "react-redux";
-import {Container, Table} from "reactstrap";
-import{Button} from "reactstrap";
-import { emptyCart } from "../Slice/cart";
-import { useNavigate } from "react-router-dom";
- export default function Index() {
-  
- const {productId} = useParams();
- console. log ("productId",productId,tabIndex);
-useEffect(()=>{},[productId,tabIndex]);
+
+
+import {v4} from "uuid";
 
 
 
+export const Slideritems = [
+    {
+      src: "/images/pinkshoe.jpg",
+      altText: "Slide 1",
+      caption: "Slide 1",
+      key: 1,
+    },
+    {
+      src: "https://picsum.photos/id/456/1200/400",
+      altText: "Slide 2",
+      caption: "Slide 2",
+      key: 2,
+    },
+    {
+      src: "https://picsum.photos/id/678/1200/400",
+      altText: "Slide 3",
+      caption: "Slide 3",
+      key: 3,
+    },
+  ];
+  export const NewArrivalData = [ {
+id: v4(),
 
 
-  return ( 
-      
-        <Container fluid>Product Details Page</Container>
-                ) ;
-}
+    name:" Shoes",
+     description:"description text", 
+     images : ["https://res.cloudinary.com/diff2ijtl/image/upload/v1724087271/cld-sample-5.jpg",
+      "https://res.cloudinary.com/diff2ijtl/image/upload/v1724087267/samples/shoe.jpg",
+      "https://res.cloudinary.com/diff2ijtl/image/upload/v1724087261/samples/ecommerce/shoes.png",
+    ], price : 2340, },
+    {
+      name:" Shoes",
+       description:"description text", 
+       images : ["https://res.cloudinary.com/diff2ijtl/image/upload/v1724087271/cld-sample-5.jpg",
+        "https://res.cloudinary.com/diff2ijtl/image/upload/v1724087267/samples/shoe.jpg",
+        "https://res.cloudinary.com/diff2ijtl/image/upload/v1724087261/samples/ecommerce/shoes.png",
+      ], price : 2340,  },
+      {
+        name:" Shoes",
+         description:"description text", 
+         images : ["https://res.cloudinary.com/diff2ijtl/image/upload/v1724087271/cld-sample-5.jpg",
+          "https://res.cloudinary.com/diff2ijtl/image/upload/v1724087267/samples/shoe.jpg",
+          "https://res.cloudinary.com/diff2ijtl/image/upload/v1724087261/samples/ecommerce/shoes.png",
+        ], price : 2340,  },
+
+        {id: v4(),          name:" Shoes",
+           description:"description text", 
+           images : ["https://res.cloudinary.com/diff2ijtl/image/upload/v1724087271/cld-sample-5.jpg",
+            "https://res.cloudinary.com/diff2ijtl/image/upload/v1724087267/samples/shoe.jpg",
+            "https://res.cloudinary.com/diff2ijtl/image/upload/v1724087261/samples/ecommerce/shoes.png",
+          ], price : 2340,  }
+        ]
+        export const BestSellingData = [ {
+          id: v4(),          name:" Shoes",
+           description:"description text", 
+           images : ["https://res.cloudinary.com/diff2ijtl/image/upload/v1724087271/cld-sample-5.jpg",
+            "https://res.cloudinary.com/diff2ijtl/image/upload/v1724087267/samples/shoe.jpg",
+            "https://res.cloudinary.com/diff2ijtl/image/upload/v1724087261/samples/ecommerce/shoes.png",
+          ], price : 2340,  },
+
+          { id: v4(), name:" Shoes",
+             description:"description text", 
+             images : ["https://res.cloudinary.com/diff2ijtl/image/upload/v1724087271/cld-sample-5.jpg",
+              "https://res.cloudinary.com/diff2ijtl/image/upload/v1724087267/samples/shoe.jpg",
+              "https://res.cloudinary.com/diff2ijtl/image/upload/v1724087261/samples/ecommerce/shoes.png",
+            ], price : 2340,  },
+            {id: v4(),              name:" Shoes",
+               description:"description text", 
+               images : ["https://res.cloudinary.com/diff2ijtl/image/upload/v1724087271/cld-sample-5.jpg",
+                "https://res.cloudinary.com/diff2ijtl/image/upload/v1724087267/samples/shoe.jpg",
+                "https://res.cloudinary.com/diff2ijtl/image/upload/v1724087261/samples/ecommerce/shoes.png",
+              ], price : 2340,  },
+              {id: v4() ,               name:" Shoes",
+                 description:"description text", 
+                 images : ["https://res.cloudinary.com/diff2ijtl/image/upload/v1724087271/cld-sample-5.jpg",
+                  "https://res.cloudinary.com/diff2ijtl/image/upload/v1724087267/samples/shoe.jpg",
+                  "https://res.cloudinary.com/diff2ijtl/image/upload/v1724087261/samples/ecommerce/shoes.png",
+                ], price : 2340,  }
+              ]
